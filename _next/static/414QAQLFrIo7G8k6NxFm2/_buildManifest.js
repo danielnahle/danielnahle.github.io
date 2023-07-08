afc05ad7e2fec4a370e40e555337683dbcc40b9e
dan <dnahle@usc.edu>
2023-07-08 16:58:10 -0700

    Update

---
+++ _next/static/414QAQLFrIo7G8k6NxFm2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/482-a7ae9e1844c1f1b2.js","static/css/fed97580c0acacee.css","static/chunks/pages/index-d87591c0f2088a68.js"],"/_error":["static/chunks/pages/_error-7fafba9435aeb6bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/dBL4fmviIk56rCJF9PXFq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/482-a7ae9e1844c1f1b2.js","static/css/fed97580c0acacee.css","static/chunks/pages/index-e01e5baf1472f9bf.js"],"/_error":["static/chunks/pages/_error-7fafba9435aeb6bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
