79f508354d8eee76da8c6bb58bbc74f263082ee6
dan <dnahle@usc.edu>
2023-07-08 14:49:50 -0700

    CSS fixes

---
+++ _next/static/o78-q1ZHFzW_FyT-hsC_H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/482-a7ae9e1844c1f1b2.js","static/css/a7fdb3691a890ad8.css","static/chunks/pages/index-6b77efdace0e2227.js"],"/_error":["static/chunks/pages/_error-7fafba9435aeb6bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
